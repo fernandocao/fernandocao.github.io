89d54643cf93bb97f96c5a66ef9b24c15716ef93
fernandocao <40540446+fernandocao@users.noreply.github.com>
2022-09-14 11:16:37 -0500

    Con categorias

--- script.js
+++ script.js
@@ -1,14 +1,96 @@
+//let mainSlider = null;
+//let navSlider = null;
+
+//Textos
+let datosSlide = {
+  frescos:[
+    {
+      titulo:"Grupo MIFA Produce",
+      subtitulo:"Limes Persian and Hass Avocados",
+      texto:"Mexican corporation committed with the enviroment and the highest quality standarts in the production and exportation of organic and conventional Persian Limes and Hass Avocados, in order to fullfill all our clients needs.",
+      fondo:"https://www.gpomifa.com/wp-content/uploads/2021/10/slider.jpeg",
+      qrPagina:"img/qr/mifa.png",
+      qrFicha:"img/qr/mifa.png",
+      logo:"img/mifa.png"
+    },
+    {
+      titulo:"Confruver",
+      subtitulo:"",
+      texto:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      fondo:"img/confruver.jpeg",
+      qrPagina:"",
+      qrFicha:"",
+      logo:"img/Comfruver.jpg"
+    },
+    {
+      titulo:"Central de mermeladas",
+      subtitulo:"",
+      texto:"Central de Mermeladas Central de Mermeladas is a Xalapa company dedicated to the production of artisanal jams, made with 100% natural ingredients, without preservatives.",
+      fondo:"img/mermelada.jpeg",
+      qrPagina:"",
+      qrFicha:"",
+      logo:"img/central.png"
+    },
+    {
+      titulo:"Bernadette	Newman",
+      subtitulo:"",
+      texto:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      fondo:"img/murrietafondo.jpeg",
+      qrPagina:"",
+      qrFicha:"",
+      logo:"img/murrieta.png"
+    },
+    {
+      titulo:"Molino de Chocolate",
+      subtitulo:"",
+      texto:"Artisan-gourmet chocolate manufacturing brand that has a presence in 143 stores at national and international level. With 5 years of experience in the market, we have been able to demonstrate that 100% natural and traditional Chocolate is and always will be preferred throughout the world.",
+      fondo:"img/fondomolino.jpeg",
+      qrPagina:"",
+      qrFicha:"",
+      logo:"img/molino.jpeg"
+    },
+    {
+      titulo:"Café Andrade",
+      subtitulo:"",
+      texto:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      fondo:"https://cafeandrade.com/wp-content/uploads/2019/06/FONDO-NUESTRO-CAF%C3%8B.jpg",
+      qrPagina:"",
+      qrFicha:"",
+      logo:"img/andrade.png"
+    },
+    {
+      titulo:"Agrover",
+      subtitulo:"",
+      texto:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      fondo:"img/agroverfondo.jpeg",
+      qrPagina:"",
+      qrFicha:"",
+      logo:"img/agrover.png"
+    },
+    {
+      titulo:"Fresh Seassons",
+      subtitulo:"",
+      texto:"Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+      fondo:"img/freshseassonsfondo.jpeg",
+      qrPagina:"",
+      qrFicha:"",
+      logo:"img/freshseasons.png"
+    }    
+  ],
+  procesados:{},
+  bebidas:{},
+}
+
 // Params
 let mainSliderSelector = '.main-slider',
     navSliderSelector = '.nav-slider',
     interleaveOffset = 0.5;
-
 // Main Slider
 let mainSliderOptions = {
       loop: true,
       speed:1000,
       autoplay:{
-        delay:30000
+        delay:3000
       },
       loopAdditionalSlides: 10,
       grabCursor: true,
@@ -60,7 +142,7 @@ let mainSliderOptions = {
         }
       }
     };
-let mainSlider = new Swiper(mainSliderSelector, mainSliderOptions);
+let mainSlider = new Swiper(mainSliderSelector, mainSliderOptions);    
 
 // Navigation Slider
 let navSliderOptions = {
@@ -84,6 +166,69 @@ let navSliderOptions = {
     };
 let navSlider = new Swiper(navSliderSelector, navSliderOptions);
 
-// Matching sliders
 mainSlider.controller.control = navSlider;
 navSlider.controller.control = mainSlider;
+
+//actualizaSlide('frescos');
+/*
+function actualizaSlide( opcion ){
+  htmlFondosSlides = '';
+  htmlLogosSlides = '';
+  datosSlide.frescos.forEach(element => {
+    htmlFondosSlides += '<div class="swiper-slide">' +
+                    '<figure class="slide-bgimg" style="background-image:url(' + element.fondo + ')"> ' +
+                      '<img src="' + element.fondo + '" class="entity-img" />' +
+                    '</figure>' +
+                    '<div class="content">' +
+                      '<p class="title">' + element.titulo + '</p>' +
+                      '<p class="subtitle">' + element.subtitulo + '</p>' +
+                      '<span class="caption">' + element.texto + '</span>' +
+                        '<br/>' +
+                        '<div class="row">' +
+                          '<div class="col-md-6">' +
+                            '<img src="' + element.qrPagina + '" />' +
+                          '</div>' +
+                          '<div class="col-md-6">' +
+                            '<img src="' + element.qrFicha + '" />' +
+                          '</div>' +
+                        '</div>' +
+                    '</div>' +
+                  '</div>';
+    htmlLogosSlides += '<div class="swiper-slide" style="background-color: darkolivegreen;">' +
+                          '<figure class="slide-bgimg" style="background-image:url(' + element.logo + ')">' +
+                            '<img src="' + element.logo + '" class="entity-img" />' +
+                          '</figure>' +
+                        '</div>';
+  });
+
+  $("#fondosSlides").html(htmlFondosSlides);
+  $("#logosSlides").html(htmlLogosSlides);    
+
+  $(".main-slider").removeClass('loading');
+  $(".nav-slider").removeClass('loading');
+
+
+
+  if ( mainSlider == null ) {
+    console.log("A");
+    mainSlider = new Swiper(mainSliderSelector, mainSliderOptions);
+  }else{
+    console.log("B");
+    mainSlider = null;
+  }
+
+  console.log( mainSlider );
+
+  if ( navSlider == null ) {
+    navSlider = new Swiper(navSliderSelector, navSliderOptions);
+  }else{
+    navSlider = null;
+  }
+
+  if ( !mainSlider == null ) {
+    // Matching sliders
+    mainSlider.controller.control = navSlider;
+    navSlider.controller.control = mainSlider;
+  }  
+}
+*/
